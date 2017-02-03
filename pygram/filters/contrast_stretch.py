f1c8f88656e4cc6873a4b543809d0e3242138b60
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-02-04 03:49:34 +0530

    lomo_fi renamed to contrast_stretch

--- pygram/filters/contrast_stretch.py
+++ pygram/filters/contrast_stretch.py
@@ -5,28 +5,33 @@ def lomo_fi(image):
 
 	# img = Image.open('/media/coding_coffee/New Volume/Projects/test_dump/Tony_Stark.jpg')
 	# img.show()
-	img = image
+	# img = image
 	img = np.asarray(img)
 	img = img.tolist()
-	
+
 	colorPtr = 0 #0-red, 1-green, 2-blue
 
 	alpha = 0.8 #<1
-	beta  = 25
+	beta  = 2.1
 	gamma = 0.6	#<1 #will use this later
 
-	for color in a:
+	for color in img:
 		row = 0
 		for location in color:
 			col = 0
 			for value in location:
-				a[colorPtr][row][col] = alpha*value + beta
+				if value<100:
+					img[colorPtr][row][col] = alpha*value
+				elif value<125:
+					img[colorPtr][row][col] = 100*alpha + beta*value
+				else:
+					img[colorPtr][row][col] = 100*alpha + 125*beta + gamma*value
 				col+=1
 			row+=1	
 		### Track color
 		colorPtr+=1
 
 	newImage = np.array(img, dtype=np.uint8)
-	newImage = Image.fromarray(newImagenewImage, 'RGB')
-	# newImageb.show()
+	newImage = Image.fromarray(newImage, 'RGB')
+	# newImage.show()
 	return newImage
